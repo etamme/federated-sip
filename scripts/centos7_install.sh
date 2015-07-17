3eead90d2fbc60ba9511089b75269a23851010d5
Eric Tamme <eric@uphreak.com>
2015-07-17 15:01:33 -0400

    knocking out little bugs

--- scripts/centos7_install.sh
+++ scripts/centos7_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # get some basica vars for later
-PWD=$(pwd)
+BASE=$(pwd)
 IP=$(ifconfig eth0 | awk '/inet /{print substr($2,1)}')
 # add opensips user with no shell
 useradd -s /bin/false opensips
@@ -17,15 +17,17 @@ cd opensips
 cp Makefile.conf.template Makefile.conf
 sed -i -e 's/include_modules?=/include_modules?= db_sqlite/g' Makefile.conf 
 sed -i -e 's/PREFIX=\/usr\/local\//PREFIX=\/usr\/local\/opensips\//g' Makefile.conf
-make all && make all install
+#make all && make all install
 # set up our sqlite database
+cd scripts/sqlite
 mkdir /var/db/opensips && chown opensips:opensips /var/db/opensips
+chown -R opensips:opensips /var/db/opensips
 sudo -u opensips sqlite3 /var/db/opensips/opensips < standard-create.sql
 sudo -u opensips sqlite3 /var/db/opensips/opensips < dialog-create.sql
 sudo -u opensips sqlite3 /var/db/opensips/opensips < domain-create.sql
 sudo -u opensips sqlite3 /var/db/opensips/opensips < auth_db-create.sql
 sudo -u opensips sqlite3 /var/db/opensips/opensips < usrloc-create.sql
-sudo -u opensips sqlite3 /var/db/opensips/opensips < $PWD/create_translations_table.sqlite
+sudo -u opensips sqlite3 /var/db/opensips/opensips < $BASE/scripts/create_translations_table.sqlite
 # set up opensipsctlrc to use our sqlite database
 sed -i -e 's/# DBENGINE=MYSQL/DBENGINE=SQLITE/g' /usr/local/opensips/etc/opensips/opensipsctlrc
 sed -i -e 's/# DB_PATH="\/usr\/local\/etc\/opensips\/dbtext"/DB_PATH=\/var\/db\/opensips\/opensips/g' /usr/local/opensips/etc/opensips/opensipsctlrc
@@ -40,12 +42,14 @@ mkdir /usr/local/rtpengine && cp rtpengine /usr/local/rtpengine/
 cd /usr/local/src/sqlite3-pcre
 make && make install
 # configure federated opensips.var.rb
-cd $PWD/core
+cd $BASE/core
 cp opensips.var.rb.sample opensips.var.rb
 # disable tls and ipv6
 sed -i -e 's/enable_tls      = true/enable_tls      = false/g' opensips.var.rb
-sed -i -e 's/enable_ipv6      = true/enable_ipv6      = false/g' opensips.var.rb
+sed -i -e 's/enable_ipv6     = true/enable_ipv6      = false/g' opensips.var.rb
 # set our listening IP address
-sed -i -e "s/listen_ip       = 'xxx.xxx.xxx.xxx'/listen_ip       = '$IP'/g" opensips.var.rb
+sed -i -e "s/listen_ip       = 'xxx.xxx.xxx.xxx'/listen_ip      = '$IP'/g" opensips.var.rb
 # set our modules directory
 sed -i -e 's#/usr/local/lib64/opensips/modules/#/usr/local/opensips/lib64/opensips/modules/#g' opensips.var.rb
+./build.rb && cp opensips.cfg /usr/local/opensips/etc/opensips/opensips.cfg
+cd /usr/local/opensips && sbin/opensips &

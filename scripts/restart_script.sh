51ea63db8f229e9553bf2410abac93cd28df1c27
Eric Tamme <eric@uphreak.com>
2015-11-03 09:53:02 -0500

    tidied up the restart script and made kernel module support configurable

--- scripts/restart_script.sh
+++ scripts/restart_script.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# set this to "true" if you have loaded BOTH the rtpengine
+# iptables module, and kernel module
+USINGKERNEL="false"
+
 # STOP
 #--------------------------------------------------------
 PID=$(pidof opensips)
-if [ -e $PID ]
+if [ -e "$PID" ]
 then 
   echo "opensips not running"
 else
@@ -15,7 +19,7 @@ else
   sleep 1
 fi
 PID=$(pidof rtpengine)
-if [ -e $PID ]
+if [ -e "$PID" ]
 then 
   echo "rtpengine not running"
 else
@@ -23,33 +27,44 @@ else
   kill -9 $PID
   sleep 1
 fi
-if [ -d "/proc/rtpengine/0" ]; then
-  echo "removing control table"
-  echo "del 0" > /proc/rtpengine/control
+
+if [ "$USINGKERNEL" == "true" ]
+then
+  if [ -d "/proc/rtpengine/0" ]; then
+    echo "removing control table"
+    echo "del 0" > /proc/rtpengine/control
+  fi
+  echo "removing iptables rules"
+  iptables -D INPUT -p udp -j rtpengine
+  iptables -D rtpengine -p udp -j RTPENGINE --id 0
+  iptables -X rtpengine
+  echo "unloading kernel module"
+  rmmod xt_RTPENGINE
 fi
-echo "removing iptables rules"
-iptables -D INPUT -p udp -j rtpengine
-iptables -D rtpengine -p udp -j RTPENGINE --id 0
-iptables -X rtpengine
-echo "unloading kernel module"
-rmmod xt_RTPENGINE
+
 sleep 1
 
 # START
 #--------------------------------------------------------
 IP=$(ifconfig eth0 | grep 'inet ' | awk '{print $2}')
-echo "loading kernel module"
-insmod /usr/local/src/rtpengine/kernel-module/xt_RTPENGINE.ko
-sleep 1
 
-echo "creating iptables rules"
-iptables -N rtpengine
-iptables -I rtpengine -p udp -j RTPENGINE --id 0
-iptables -I INPUT -p udp -j rtpengine
-sleep 1
+if [ "$USINGKERNEL" == "true" ]
+then
+  echo "loading kernel module"
+  insmod /usr/local/src/rtpengine/kernel-module/xt_RTPENGINE.ko
+  sleep 1
 
-echo "starting rtpengine"
-/usr/local/rtpengine/rtpengine -p /var/run/rtpengine.pid --interface $IP --listen-ng $IP:60000 -m 50000 -M 55000 -L 7 --table=0 --log-facility=local3
+  echo "creating iptables rules"
+  iptables -N rtpengine
+  iptables -I rtpengine -p udp -j RTPENGINE --id 0
+  iptables -I INPUT -p udp -j rtpengine
+  sleep 1
+
+  echo "starting rtpengine"
+  /usr/local/rtpengine/rtpengine -p /var/run/rtpengine.pid --interface $IP --listen-ng $IP:60000 -m 50000 -M 55000 -L 7 --table=0 --log-facility=local3
+else
+  /usr/local/rtpengine/rtpengine -p /var/run/rtpengine.pid --interface $IP --listen-ng $IP:60000 -m 50000 -M 55000 -L 7 --log-facility=local3
+fi
 sleep 1
 
 echo "starting opensips"

c87c2c2226d5da0feb984985cec386a37113c61f
Eric Tamme <eric@uphreak.com>
2016-02-08 13:59:05 -0700

    move source building into yaml playbook

--- files/build_rtpengine.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /usr/local/src
-git clone https://github.com/sipwise/rtpengine.git
-cd /usr/local/src/rtpengine/daemon
-git checkout -t origin/mr4.0.1
-make
-mkdir /usr/local/rtpengine && cp rtpengine /usr/local/rtpengine/
--- files/build_sqlite-pcre.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git clone https://github.com/ralight/sqlite3-pcre.git
-cd /usr/local/src/sqlite3-pcre
-make && make install
